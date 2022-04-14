f1c7a77d5afd652302744f508b1af9f60996974c
adelaweidischova <48671822+adelaweidischova@users.noreply.github.com>
2022-04-14 08:17:49 +0000

    Deploying to docs from @ books-are-next/pabitele@1f9fdfd7468876f7b0e8baa8fad562fc2a0c5d6e 🚀

--- epub2nb-output/content/_book.md
+++ epub2nb-output/content/_book.md
@@ -2,18 +2,19 @@
 outputs: meta
 slug: book
 contentType: prose
-languageCode: en
+languageCode: cs
 meta:
+  languageCode: cs
+  contentType: prose
   title: Pábitelé
   identifier: urn:uuid:36a18549-757d-472f-94c0-eac8c5d3978f
   author: Bohumil Hrabal
   publisher: Městská knihovna v Praze
   modified: '2019-09-03T00:00:00.000Z'
+  edition: beletrie
+  publisherShort: mkp
 readingOrder:
-  - pabitele_001.html
   - pabitele_002.html
-  - pabitele_003.html
-  - pabitele_004.html
   - pabitele_005.html
   - pabitele_006.html
   - pabitele_007.html
@@ -26,12 +27,49 @@ readingOrder:
   - pabitele_014.html
   - pabitele_015.html
   - pabitele_016.html
-  - pabitele_017.html
   - pabitele_018.html
+  - colophon.html
 tocBase:
   - id: section-1
-    children: []
+    children:
+      - link: pabitele_005.html
+        title: Citát
+        hiddenTitle: true
+      - link: pabitele_006.html
+        title: Jarmilka
+        hiddenTitle: false
+      - link: pabitele_007.html
+        title: Pan notář
+        hiddenTitle: false
+      - link: pabitele_008.html
+        title: Pohřeb
+        hiddenTitle: false
+      - link: pabitele_009.html
+        title: Bambini di Praga 1947
+        hiddenTitle: false
+      - link: pabitele_010.html
+        title: Pábitelé
+        hiddenTitle: false
+      - link: pabitele_011.html
+        title: Automat Svět
+        hiddenTitle: false
+      - link: pabitele_012.html
+        title: Chcete vidět zlatou Prahu?
+        hiddenTitle: false
+      - link: pabitele_013.html
+        title: Iontoforéza
+        hiddenTitle: false
+      - link: pabitele_014.html
+        title: "Dáma s\_kaméliemi"
+        hiddenTitle: false
+      - link: pabitele_015.html
+        title: Diamantové očko
+        hiddenTitle: false
+      - link: pabitele_016.html
+        title: Romance
+        hiddenTitle: false
     isSection: true
+    listType: plain
 static:
   - style
   - scripts
---
+++ service-worker.js
@@ -0,0 +1,60 @@
+/* eslint-disable no-restricted-globals */
+
+/* global self, caches, fetch */
+
+const CACHE = 'cache-1f9fdfd';
+
+self.addEventListener('install', e => {
+  e.waitUntil(precache()).then(() => self.skipWaiting());
+});
+
+self.addEventListener('activate', event => {
+  self.clients
+    .matchAll({
+      includeUncontrolled: true,
+    })
+    .then(clientList => {
+      const urls = clientList.map(client => client.url);
+      console.log('[ServiceWorker] Matching clients:', urls.join(', '));
+    });
+
+  event.waitUntil(
+    caches
+      .keys()
+      .then(cacheNames =>
+        Promise.all(
+          cacheNames.map(cacheName => {
+            if (cacheName !== CACHE) {
+              console.log('[ServiceWorker] Deleting old cache:', cacheName);
+              return caches.delete(cacheName);
+            }
+            return null;
+          })
+        )
+      )
+      .then(() => {
+        console.log('[ServiceWorker] Claiming clients for version', CACHE);
+        return self.clients.claim();
+      })
+  );
+});
+
+function precache() {
+  return caches.open(CACHE).then(cache => cache.addAll(["./","./colophon.html","./favicon.png","./index.html","./manifest.json","./pabitele_002.html","./pabitele_006.html","./pabitele_007.html","./pabitele_008.html","./pabitele_009.html","./pabitele_010.html","./pabitele_011.html","./pabitele_012.html","./pabitele_013.html","./pabitele_014.html","./pabitele_005.html","./pabitele_015.html","./pabitele_016.html","./pabitele_018.html","./fonts/Literata-Italic-var.woff2","./fonts/Literata-var.woff2","./fonts/LiterataTT-TextItalic.woff2","./fonts/LiterataTT-TextSemibold.woff2","./fonts/LiterataTT_LICENSE.txt","./fonts/LiterataTT-TextRegular.woff2","./fonts/SpaceGroteskVF.woff2","./fonts/SpaceGroteskVF_LICENSE.txt","./resources/image001_fmt.jpeg","./resources/image002_fmt.jpeg","./resources/kocka.jpeg","./resources/obalka_pabitele_fmt.jpeg","./resources/upoutavka_eknihy_fmt.jpeg","./scripts/bundle.js","./style/style.min.css","./template-images/circles.png"]));
+}
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.open(CACHE).then(cache => {
+      return cache.match(e.request).then(matching => {
+        if (matching) {
+          console.log('[ServiceWorker] Serving file from cache.');
+          console.log(e.request);
+          return matching;
+        }
+
+        return fetch(e.request);
+      });
+    })
+  );
+});
